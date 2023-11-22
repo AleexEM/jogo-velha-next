77b1a108a38710dd2b1a653e4be029db08e46c7d
AleexEM <alexmeurer3@gmail.com>
2023-11-22 16:52:43 -0300

    primeiro

---
+++ app/page.tsx
@@ -0,0 +1,34 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main>
+
+
+
+      <div>       
+        <table className='flex items-center justify-center my-28'>
+          <tbody>
+            <tr>
+              <td className='w-20 h-20 text-7xl text-red-600 bg-orange-950 border-8 hover:bg-orange-900'></td>
+              <td className='w-20 h-20 text-7xl text-red-600 bg-orange-950 border-8 hover:bg-orange-900'></td>
+              <td className='w-20 h-20 text-7xl text-red-600 bg-orange-950 border-8 hover:bg-orange-900'></td>
+            </tr>
+            <tr>
+              <td className='w-20 h-20 text-7xl text-red-600 bg-orange-950 border-8 hover:bg-orange-900'></td>
+              <td className='w-20 h-20 text-7xl text-red-600 bg-orange-950 border-8 hover:bg-orange-900'></td>
+              <td className='w-20 h-20 text-7xl text-red-600 bg-orange-950 border-8 hover:bg-orange-900'></td>
+            </tr>
+            <tr>
+              <td className='w-20 h-20 text-7xl text-red-600 bg-orange-950 border-8 hover:bg-orange-900'></td>
+              <td className='w-20 h-20 text-7xl text-red-600 bg-orange-950 border-8 hover:bg-orange-900'></td>
+              <td className='w-20 h-20 text-7xl text-red-600 bg-orange-950 border-8 hover:bg-orange-900'></td>
+            </tr>
+          </tbody>
+        </table>
+
+      </div>
+
+    </main>
+  )
+}
